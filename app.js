8890f417ecd454b5fcbd2207dbdf7205ad5c5603
임채연 <dlacodus0407@gmail.com>
2023-07-14 03:39:56 +0900

    템플릿

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const app = require('./src/config/express');
 const env = process.env;
 
 app.listen(env.SERVER_PORT, () => 
-    console.log('server open ' + process.env.SERVER_PORT
+    console.log('server open!!' + process.env.SERVER_PORT
 ))
