3a9626b6114e6f8a78fe97593b94e68553549dc5
임채연 <dlacodus0407@gmail.com>
2023-07-15 01:40:05 +0900

    board 삭제

--- src/routers/board.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/BoardController')
-
-router.post('/', ctrl.process.upload);
-
-
-module.exports = router;
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const ctrl = require('../controllers/homeController');
 
 router.get('/', ctrl.output.home);
-router.use('/board', require('../routers/board'));
 router.use('/kickboard', require('../routers/kickBoard'));
 
 module.exports = router;
